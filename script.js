a701380fc3c4aee3c4ee2a769a0599c5a5434096
Dhruv <narayandhruv927@gmail.com>
2026-09-03 16:52:48 +0530

    Adding Functionalities

---
+++ script.js
@@ -0,0 +1,19 @@
+let smallImg = [
+    {
+        image:""
+    },
+     {
+        image:""
+    },
+     {
+        image:""
+    },
+     {
+        image:""
+    },
+     {
+        image:""
+    },
+]
+let cartItems = document.querySelector('.cart-items')
+let main = document.querySelector('main');
