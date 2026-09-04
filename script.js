e6dcfb1e79be7e5701bd561913071035b9151e13
Dhruv <narayandhruv927@gmail.com>
2026-09-04 18:39:29 +0530

    cart-items adding

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
-let smallImg = [
+let productImg = [
     {
-        image:""
+        name:"Black Buffer",
+        image:"./product2.png",
+        price:"$100",
     },
      {
-        image:""
+         name:"Black Buffer",
+        image:"./product3.png",
+        price:"$100",
     },
      {
-        image:""
+     name:"Black Buffer",
+        image:"./product4.png",
+        price:"$100",
     },
      {
-        image:""
-    },
-     {
-        image:""
+         name:"Black Buffer",
+        image:"./product5.png",
+        price:"$100",
     },
 ]
 let cartItems = document.querySelector('.cart-items')
-let main = document.querySelector('main');
+let cart = document.querySelector('#cart')
+let MainImg = document.querySelector('#MainImg');
+cart.addEventListener('click',function(){
+    cartItems.style.visibility = "visible";
+})
+let currentProduct = productImg[0];
+let cartProducts = [];
+let clutter = '';
+function cartRender(){
+cartProducts.forEach(function(ProductRender){
+
+ clutter += `<img src="${ProductRender.image}" alt="">
+            <div class="product-info">
+            <h4>${ProductRender.name}</h4>
+            <h3>${ProductRender.price}</h3>
+            </div>`
+
+
+cartItems.innerHTML = clutter;
+
+})
+}
+document.querySelector('.look').addEventListener('click',function(){
+
+cartProducts.push(currentProduct);
+cartRender();
+})
